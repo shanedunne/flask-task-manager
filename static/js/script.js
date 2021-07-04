65423b1c7bf2899c8155dd4bebe3d71143a2607a
shanedunne <shanejjdunne@hotmail.com>
2021-07-04 09:59:53 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
+$(document).ready(function () {
+  $(".sidenav").sidenav({edge: "right"});
+  $(".collapsible").collapsible();
+  $(".tooltipped").tooltip();
+  $(".datepicker").datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
   });
-
-$(document).ready(function(){
-  $('.collapsible').collapsible();
- });
-
- $(document).ready(function(){
-  $('.tooltipped').tooltip();
 });
